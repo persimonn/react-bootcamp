30ac73eaaab73c14bdcd30aae75a678168f0e5e0
Student <Student@LVAGPLTP0682_BC.dir.svc.accenture.com>
2018-08-24 14:38:29 +0300

    formating

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
 import PostCard from "../postCards/index.js";
-import catImg1 from "../../media/bwcat1.jpg";
-import catImg2 from "../../media/bwcat2.jpg";
-import catImg3 from "../../media/bwcat3.jpg";
-import catImg4 from "../../media/bwcat4.jpg";
-import catImg5 from "../../media/bwcat5.jpg";
 
 class Posts extends Component {
   componentDidMount() {
